d4b50952cdae60df7c710119808bcdcbf62596d5
Charles-MAD <charlesdang00@gmail.com>
2024-01-12 01:03:04 +0200

    Ajout de la directive use strict

---
+++ repeter.js
@@ -0,0 +1,6 @@
+// Ecrivons une boucle qui demande un nombre supérieur à 100.
+'use strict';
+do {
+  let number = +prompt("Entrez un nombre supérieur à 100", 1);
+//   if (number < 100);
+} while(number < 100);
