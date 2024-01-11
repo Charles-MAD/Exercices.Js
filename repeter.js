75950ef29cba382b0189fe47bfb3ade30e83e72b
Charles-MAD <charlesdang00@gmail.com>
2024-01-12 01:38:31 +0200

    Ajout du fichier repeter.js

--- repeter.js
+++ repeter.js
@@ -1,6 +1,13 @@
 // Ecrivons une boucle qui demande un nombre supérieur à 100.
 'use strict';
+let number;
+let test = true;
+
 do {
   let number = +prompt("Entrez un nombre supérieur à 100", 1);
-//   if (number < 100);
-} while(number < 100);
+  if (number <= 100) {
+    test = true;
+  } else {
+    test = false;
+  }
+} while(test);
