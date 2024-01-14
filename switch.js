7a5ae206519a061c0ca872525383613ebd31878f
Charles-MAD <charlesdang00@gmail.com>
2024-01-14 12:16:40 +0200

    Remplacement de alert() par console.log()

--- switch.js
+++ switch.js
@@ -1,16 +1,32 @@
 'use strict';
+// let a = 2 + 2;
+// // exemple de cas d'utilisation de la declaration "switch"
+
+// switch (a) {
+//     case 'value1': // si (a === 'value1')
+//     ...
+//     [break]
+
+//     case 'value2': // si (a === 'value2')
+//     ...
+//     break
+
+//     default: 
+//     ...
+//     [break]
+// }
+
+// Exemple de code avec switch
+
 let a = 2 + 2;
-// exemple de cas d'utilisation de la declaration "switch"
+
 switch (a) {
-    case 3:
-        console.log( 'Too small' );
-        break;
+    case 3: 
+        console.log('Too small');
     case 4:
-        console.log( 'Exactly!');
-        break;
+        console.log('Exactly');
     case 5:
-        console.log( 'Too big' );
-        break;
+        console.log('Too big');
     default:
-        console.log( "I don't know such values");
+        console.log("I don't know such values");
 }
