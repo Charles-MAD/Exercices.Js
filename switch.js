50f408acbe5accdfab8a5ebf309b2c8f0858684a
Charles-MAD <charlesdang00@gmail.com>
2024-01-14 12:39:57 +0200

    Modification des expressions en anglais

--- switch.js
+++ switch.js
@@ -1,6 +1,6 @@
 'use strict';
 // let a = 2 + 2;
-// // exemple de cas d'utilisation de la declaration "switch"
+// example of use case of "switch" declaration
 
 // switch (a) {
 //     case 'value1': // si (a === 'value1')
@@ -16,17 +16,45 @@
 //     [break]
 // }
 
-// Exemple de code avec switch
+// Example the code with switch
+
+// let a = 2 + 2;
+
+// switch (a) {
+//     case 3: 
+//         console.log('Too small');
+//     case 4:
+//         console.log('Exactly');
+//     case 5:
+//         console.log('Too big');
+//     default:
+//         console.log("I don't know such values");
+// }
+
+// Having an expression, it can be a switch/case argument
+// let a = "1";
+// let b = 0;
+
+// switch(+a) {
+//     case b + 1:
+//         console.log("this runs, because +a is 1, exactly equals b+1");
+//         break;
+//     default:
+//         console.log("this doesn't run");
+// }
+
+// If we have several boxes that share the same code, we can group them.
 
 let a = 2 + 2;
 
-switch (a) {
-    case 3: 
-        console.log('Too small');
+switch(a) {
     case 4:
-        console.log('Exactly');
+        console.log('Rigth!');
+        break;
+
+    case 3: // grouping of two cases
     case 5:
-        console.log('Too big');
-    default:
-        console.log("I don't know such values");
+        console.log('Wrong!');
+        console.log("Why don't you take a math class?");
+        break;
 }
